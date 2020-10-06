19cdfb4c0da976ab892ca455a67cb27d793f2877
jake_lyu <gomgom3409@gmail.com>
2020-10-06 15:01:14 +1100

    Updates

---
+++ precache-manifest.b97694accbeb48ab50ec5fd1971b4761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d2b0523946a8da7efdeab4dbe11110",
+    "url": "/youtube_clone_hook/index.html"
+  },
+  {
+    "revision": "f2c3408c8eaeb8cee88c",
+    "url": "/youtube_clone_hook/static/css/main.b5f25ded.chunk.css"
+  },
+  {
+    "revision": "f20536e8257119519690",
+    "url": "/youtube_clone_hook/static/js/2.5d256285.chunk.js"
+  },
+  {
+    "revision": "746582258d70325461391e08406982a5",
+    "url": "/youtube_clone_hook/static/js/2.5d256285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c3408c8eaeb8cee88c",
+    "url": "/youtube_clone_hook/static/js/main.5c2fd5ca.chunk.js"
+  },
+  {
+    "revision": "0131b724d74e612f0ed6",
+    "url": "/youtube_clone_hook/static/js/runtime-main.82cdc54b.js"
+  }
+]);
